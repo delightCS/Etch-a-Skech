ad6a81497d24cd396d4c8454b461a8884c9d15a2
Chinenye <badnenye@gmail.com>
2024-09-05 17:53:06 +0100

    Add a drag function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const container = document.querySelector(".container");
 
+let isDragging = false;
+
 for (i = 0; i < 16 * 16; i++) {
   const cell = document.createElement("div");
   cell.setAttribute("style", "border: 1px solid gray; height: 37.5px");
   container.appendChild(cell);
+
+  cell.addEventListener("mousedown", function (event) {
+    isDragging = true;
+  });
+
+  cell.addEventListener("mousemove", function (event) {
+    if (isDragging) {
+      cell.style.backgroundColor = "blue";
+    }
+  });
+
+  cell.addEventListener("mouseup", function () {
+    if (isDragging) {
+      isDragging = false;
+    }
+  });
 }
