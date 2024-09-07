493848932ca1f0a9d1ff1e4ac1b61b50153a0164
Chinenye <badnenye@gmail.com>
2024-09-07 21:01:05 +0100

    Add cell number change

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
-// const container = document.querySelector(".container");
-
-// let isDragging = false;
-
-// const colorInput = document.querySelector("input");
-
-// const btn = document.querySelector("button");
-
-// for (let i = 0; i < 16 * 16; i++) {
-//   const cell = document.createElement("div");
-//   cell.setAttribute("style", "border: 1px solid gray; height: 37.5px");
-//   container.appendChild(cell);
-
-//   cell.addEventListener("mousedown", function (event) {
-//     isDragging = true;
-//   });
-
-//   cell.addEventListener("mousemove", function (event) {
-//     if (isDragging) {
-//       cell.style.backgroundColor = colorInput.value;
-//     }
-//   });
-
-//   cell.addEventListener("mouseup", function () {
-//     if (isDragging) {
-//       isDragging = false;
-//     }
-//   });
-
 const container = document.querySelector(".container");
-const getCell = document.querySelector("input");
 
-let size = b;
-for (let i = 0; i < size; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  container.appendChild(cell);
+function changeCellNum() {
+  const input = document.querySelector("input").value;
+
+  if (input >= 1 && input <= 100) {
+    document.documentElement.style.setProperty("--divisor", input);
+    container.innerHTML = "";
+
+    const totalCell = input ** 2;
+    for (let i = 0; i < totalCell; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      container.appendChild(cell);
+    }
+  } else {
+    alert("Input a number between 1 and 100.");
+  }
 }
+
+const createGrid = document.querySelector("button");
+createGrid.addEventListener("click", changeCellNum);
