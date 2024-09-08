db52bcbc939c1275324c74fce6c0cb5bee78de00
Chinenye <badnenye@gmail.com>
2024-09-08 13:28:52 +0100

    Make 16 grid default

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const container = document.querySelector(".container");
 
+function creategrid(gridSize) {
+  document.documentElement.style.setProperty("--divisor", gridSize);
+  container.innerHTML = "";
+
+  const totalCell = gridSize ** 2;
+  for (let i = 0; i < totalCell; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
+}
+
 function changeCellNum() {
-  const input = document.querySelector("input").value;
+  const numInput = document.querySelector("input").value;
 
-  if (input >= 1 && input <= 100) {
-    document.documentElement.style.setProperty("--divisor", input);
-    container.innerHTML = "";
-
-    const totalCell = input ** 2;
-    for (let i = 0; i < totalCell; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      container.appendChild(cell);
-    }
+  if (numInput >= 1 && numInput <= 100) {
+    creategrid(numInput);
   } else {
     alert("Input a number between 1 and 100.");
   }
 }
 
-const createGrid = document.querySelector("button");
-createGrid.addEventListener("click", changeCellNum);
+const createGridButton = document.querySelector("button");
+createGridButton.addEventListener("click", changeCellNum);
+
+document.addEventListener("DOMContentLoaded", () => {
+  creategrid(16);
+});
